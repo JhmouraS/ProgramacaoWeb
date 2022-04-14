daf513de6b875788402d82014fa64ce6bd57dcd2
JhmouraS <101271081+JhmouraS@users.noreply.github.com>
2022-04-13 23:10:40 -0300

    Add files via upload

---
+++ AulaProgramacaoWeb/src/main/java/exservlet/ServletOlaPersonalizado.java
@@ -0,0 +1,44 @@
+package exservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletOlaPersonalizado
+ */
+@WebServlet("/ServletOlaPersonalizado")
+public class ServletOlaPersonalizado extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletOlaPersonalizado() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter saida = response.getWriter();
+		saida.write("<HTML><BODY>");
+		saida.write("Olá! "  + request.getParameter("nome") + "!!!");
+		saida.write("</BODY></HTML>");
+		saida.close();
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+}
---
+++ AulaProgramacaoWeb/src/main/java/exservlet/ServletTeste.java
@@ -0,0 +1,36 @@
+package exservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletTeste
+ */
+@WebServlet("/ServletTeste")
+public class ServletTeste extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletTeste() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	PrintWriter saida = response.getWriter();
+		saida.write("<HTML><BODY>");
+		saida.write("Olá! "  +request.getParameter("João Henrique")+ "!!!");
+		saida.write("</BODY></HTML>");
+		saida.close();
+		
+    }
+}
